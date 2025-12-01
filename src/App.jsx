87741f3453fc0a559b5999fa08f8eb47889e9867
TanvirfahadBd <perpc1122@gmail.com>
2025-12-01 12:35:23 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./app.scss"
+
+const App = () => {
+  return (
+    <div>
+      <section>Hero</section>
+      <section>peralax</section>
+      <section>Services</section>
+      <section>project1</section>
+      <section>project2</section>
+      <section>project3</section>
+      <section>project4</section>
+      <section>contact</section>
+    </div>
+  )
+}
+
+export default App
